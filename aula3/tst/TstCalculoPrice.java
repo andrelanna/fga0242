a074a5a592d0729093d9bbe0c85863c60224173d
Lieverton <lievertom@gmail.com>
2020-09-18 00:54:10 -0300

    Create TestesFuncionais Test Suite

--- aula3/tst/TstCalculoPrice.java
+++ aula3/tst/TstCalculoPrice.java
@@ -13,17 +13,6 @@ import financeiroExceptions.PrestacaoMaiorQueMargemException;
 
 @RunWith(Parameterized.class)
 public class TstCalculoPrice {
-	
-	@Parameters
-	public static Iterable<Object[]> getParameters() {
-		Object[][] dados = new Object[][] {
-			{"Banco 1", 10000f, 10, 0.02f, 1113.26f, 1132.65f},
-			{"Banco 2", 10000f, 12, 0.018f, 934.01f, 1208.23f},
-			{"Banco 3", 10000f, 15, 0.017f, 760.89f, 1413.23f},
-			{"Banco 2", 10000f, 20, 0.015f, 582.45f, 1649.14f}			
-		};
-		return Arrays.asList(dados);
-	}
 
 	private String banco;
 	private float pv;
@@ -41,12 +30,23 @@ public class TstCalculoPrice {
 		this.totalJurosEsperado = totalJurosEsperado;
 		
 	}
+	
+	@Parameters
+	public static Iterable<Object[]> getParameters() {
+		Object[][] dados = new Object[][] {
+			{"Banco 1", 10000f, 10, 0.02f, 1113.26f, 1132.65f},
+			{"Banco 2", 10000f, 12, 0.018f, 934.01f, 1208.23f},
+			{"Banco 3", 10000f, 15, 0.017f, 760.89f, 1413.23f},
+			{"Banco 2", 10000f, 20, 0.015f, 582.45f, 1649.14f}			
+		};
+		return Arrays.asList(dados);
+	}
 
 	@Test
 	@Category(TstFuncional.class)
 	public void testePrice() throws PrestacaoMaiorQueMargemException {
 		Modalidade simulacao = Modalidade.criarSimulacao("Price", banco, i);
-		Financiamento financiamento = Financiamento.criarFinanciamento(pv, n, 5000);
+		Financiamento financiamento = Financiamento.criarFinanciamento(pv, n, 2000);
 		financiamento.adicionarSimulacao(simulacao);
 		simulacao.adicionarFinanciamento(financiamento);
 		float prestacao = simulacao.calcularPrestacoes();

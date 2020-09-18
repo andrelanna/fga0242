11db3a1d51f5ad612877b7d2253fa96a0a32dbd9
Lieverton <lievertom@gmail.com>
2020-09-18 00:03:10 -0300

    Create TstCauloPrice Patameterized Test Case

---
+++ aula3/tst/TstCalculoPrice.java
@@ -0,0 +1,58 @@
+package tst;
+
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import Financeiro.Financiamento;
+import Financeiro.Modalidade;
+import financeiroExceptions.PrestacaoMaiorQueMargemException;
+
+@RunWith(Parameterized.class)
+public class TstCalculoPrice {
+	
+	@Parameters
+	public static Iterable<Object[]> getParameters() {
+		Object[][] dados = new Object[][] {
+			{"Banco 1", 10000f, 10, 0.02f, 1113.26f, 1132.65f},
+			{"Banco 2", 10000f, 12, 0.018f, 934.01f, 1208.23f},
+			{"Banco 3", 10000f, 15, 0.017f, 760.89f, 1413.23f},
+			{"Banco 2", 10000f, 20, 0.015f, 582.45f, 1649.14f}			
+		};
+		return Arrays.asList(dados);
+	}
+
+	private String banco;
+	private float pv;
+	private int n;
+	private float i;
+	private float parcelaEsperada;
+	private float totalJurosEsperado;
+	
+	public TstCalculoPrice(String banco, float pv, int n, float i, float parcelaEsperada, float totalJurosEsperado) {
+		this.banco = banco;
+		this.pv = pv;
+		this.n = n;
+		this.i = i;
+		this.parcelaEsperada = parcelaEsperada;
+		this.totalJurosEsperado = totalJurosEsperado;
+		
+	}
+
+	@Test
+	@Category(TstFuncional.class)
+	public void testePrice() throws PrestacaoMaiorQueMargemException {
+		Modalidade simulacao = Modalidade.criarSimulacao("Price", banco, i);
+		Financiamento financiamento = Financiamento.criarFinanciamento(pv, n, 5000);
+		financiamento.adicionarSimulacao(simulacao);
+		simulacao.adicionarFinanciamento(financiamento);
+		float prestacao = simulacao.calcularPrestacoes();
+		float juros = simulacao.calcularTotalJuros();
+		assertEquals(parcelaEsperada, prestacao, 2);
+		assertEquals(totalJurosEsperado, juros, 2);
+	}
+
+}
--- aula3/tst/TstInformacoesFinanciamento.java
+++ aula3/tst/TstInformacoesFinanciamento.java
@@ -1,10 +1,8 @@
 package tst;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-
 import Financeiro.Financiamento;
 import financeiroExceptions.PrestacaoMaiorQueMargemException;
 

2799df6349700f2b2dd8de1c944bbe9123da56bf
Andre Lanna <andre.lanna@gmail.com>
2021-08-11 15:48:50 -0300

    Exercicio de fixacao - revisao de testes unitários

--- aula3/financeiroExceptions/PrestacaoMaiorQueMargemException.java
+++
@@ -1,9 +0,0 @@
-package financeiroExceptions;
-
-public class PrestacaoMaiorQueMargemException extends Exception {
-
-	public PrestacaoMaiorQueMargemException(float f, float margem) {
-		super("O valor da prestacao ( " + f + " ) é maior do que o valor permitido pela margem ( " + margem + ")");
-	}
-	
-}

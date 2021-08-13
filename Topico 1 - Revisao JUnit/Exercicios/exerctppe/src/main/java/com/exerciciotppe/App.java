9d6fa971eccd2823782017eca832cc918b06726a
Nícalo Ribeiro <nicalo63@gmail.com>
2021-08-13 15:44:30 -0300

    Projeto base

---
+++ Topico 1 - Revisao JUnit/Exercicios/exerctppe/src/main/java/com/exerciciotppe/App.java
@@ -0,0 +1,17 @@
+package com.exerciciotppe;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

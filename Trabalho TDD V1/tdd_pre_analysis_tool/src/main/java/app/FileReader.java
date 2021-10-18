d7be5298e03a2d3840d839e07b12526a5c069669
pAmanda <amandoca_779@hotmail.com>
2021-10-18 15:34:06 -0300

    Adding file reader implementation
    Co-authored-by: gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
    Co-authored-by: filypsdias <filipedias1708@gmail.com>

--- Trabalho TDD V1/tdd_pre_analysis_tool/src/main/java/app/FileReader.java
+++ Trabalho TDD V1/tdd_pre_analysis_tool/src/main/java/app/FileReader.java
@@ -2,6 +2,7 @@ package main.java.app;
 
 import main.java.app.exception.ArquivoNaoEncontradoException;
 import main.java.app.exception.DelimitadorInvalidoException;
+import main.java.app.exception.DisposicaoInvalidaException;
 import main.java.app.exception.EscritaNaoPermitidaException;
 
 import java.io.*;
@@ -12,8 +13,65 @@ public class FileReader {
 	                     final String outputPath) throws IOException,
 	                                                     ArquivoNaoEncontradoException,
 	                                                     DelimitadorInvalidoException,
-	                                                     EscritaNaoPermitidaException {
+	                                                     EscritaNaoPermitidaException, DisposicaoInvalidaException {
+		File file = new File(path);
 		
+		//verify if file exists
+		if (!file.exists()) {
+			throw new ArquivoNaoEncontradoException("File not found on path: " + path);
+		}
+		
+		//verify delimiter length
+		if (delimiter.length() > 1) {
+			throw new DelimitadorInvalidoException("Delimiter should not have more than 1 character: " + delimiter);
+		}
+		
+		if (disposition.isEmpty()
+		    || (!disposition.equalsIgnoreCase("colunas")
+		        && !disposition.equalsIgnoreCase("linhas"))) {
+			
+			throw new DisposicaoInvalidaException(
+							"Invalid disposition: " + disposition + ". It should be 'linhas' or 'colunas'");
+		}
+		
+		//verify if file is writable
+		File outputFile = new File(outputPath);
+		outputFile.createNewFile();
+		
+		if (!outputFile.canWrite()) {
+			throw new EscritaNaoPermitidaException("File not writable on path: " + path);
+		}
+		
+		String st;
+		String evolution;
+		
+		int counter = 1;
+		
+		BufferedReader br = new BufferedReader(new java.io.FileReader(file));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath, false));
+		
+		while ((st = br.readLine()) != null) {
+			if (!st.contains("-")) {
+				writer.append(";");
+				writer.append(st);
+			} else {
+				final String pattern = "[^0-9]";
+				final String emptyReplacement = "";
+				
+				if (counter > 1 && st.contains("-")) {
+					writer.append(System.lineSeparator());
+					evolution = st.replaceAll(pattern, emptyReplacement);
+					writer.append(evolution);
+					counter++;
+				} else if (counter == 1) {
+					evolution = st.replaceAll(pattern, emptyReplacement);
+					writer.append(evolution);
+					counter++;
+				}
+			}
+		}
+		
+		writer.close();
 	}
 	
 }
--- Trabalho TDD V1/tdd_pre_analysis_tool/src/test/FileReaderTest.java
+++ Trabalho TDD V1/tdd_pre_analysis_tool/src/test/FileReaderTest.java
@@ -22,16 +22,20 @@ public class FileReaderTest {
 	                                            "/tdd_pre_analysis_tool/src/main/resources/analysisTime.out";
 	private static String OUTPUT_PATH = System.getProperty("user.dir") +
 	                                    "/tdd_pre_analysis_tool/src/main/resources/analysisTimeTab.out";
+	private static String OUTPUT_READONLY_PATH = System.getProperty("user.dir") +
+	                                    "/tdd_pre_analysis_tool/src/main/resources/readOnly.out";
 	
 	@Test(expected = ArquivoNaoEncontradoException.class)
 	public void fileNotFoundShouldThrowException() throws IOException, ArquivoNaoEncontradoException,
-	                                                      DelimitadorInvalidoException, EscritaNaoPermitidaException {
+	                                                      DelimitadorInvalidoException, EscritaNaoPermitidaException,
+	                                                      DisposicaoInvalidaException {
 		fileReader.loadFile(NOT_EXISTING_PATH, DELIMITER, "colunas", OUTPUT_PATH);
 	}
 	
 	@Test(expected = DisposicaoInvalidaException.class)
 	public void dispositionInvalidException() throws IOException, ArquivoNaoEncontradoException,
-	                                                 DelimitadorInvalidoException, EscritaNaoPermitidaException {
+	                                                 DelimitadorInvalidoException, EscritaNaoPermitidaException,
+	                                                 DisposicaoInvalidaException {
 		
 		fileReader.loadFile(EXISTING_PATH,
 		                    DELIMITER, "qualquercoisa", OUTPUT_PATH);
@@ -40,32 +44,34 @@ public class FileReaderTest {
 	@Test(expected = EscritaNaoPermitidaException.class)
 	public void fileWithoutPermissionToWriteShouldThrowException() throws IOException, ArquivoNaoEncontradoException,
 	                                                                      DelimitadorInvalidoException,
-	                                                                      EscritaNaoPermitidaException {
+	                                                                      EscritaNaoPermitidaException,
+	                                                                      DisposicaoInvalidaException {
 		
-		File readOnlyFile = new File(EXISTING_PATH);
+		File readOnlyFile = new File(OUTPUT_READONLY_PATH);
+		readOnlyFile.createNewFile();
 		
 		readOnlyFile.setReadOnly();
-		fileReader.loadFile(EXISTING_PATH, readOnlyFile.getPath(), DELIMITER, OUTPUT_PATH);
-		readOnlyFile.setReadable(true);
+		fileReader.loadFile(EXISTING_PATH, DELIMITER, "linhas", OUTPUT_READONLY_PATH);
+		readOnlyFile.delete();
 	}
 	
 	@Test(expected = DelimitadorInvalidoException.class)
 	public void invalidDelimiterShouldThrowException() throws IOException, ArquivoNaoEncontradoException,
-	                                                          DelimitadorInvalidoException, EscritaNaoPermitidaException {
+	                                                          DelimitadorInvalidoException, EscritaNaoPermitidaException,
+	                                                          DisposicaoInvalidaException {
 		
 		fileReader.loadFile(EXISTING_PATH, INVALID_DELIMITER, "colunas", OUTPUT_PATH);
 	}
 	
 	@Test
-	public void processFile() throws ArquivoNaoEncontradoException, EscritaNaoPermitidaException,
-	                                 DelimitadorInvalidoException, IOException {
+	public void processSuccessFile() throws ArquivoNaoEncontradoException, EscritaNaoPermitidaException,
+	                                 DelimitadorInvalidoException, IOException, DisposicaoInvalidaException {
 		
 		fileReader.loadFile(EXISTING_PATH, DELIMITER, "colunas", OUTPUT_PATH);
 		
 		File file = new File(OUTPUT_PATH);
 		Assert.assertNotNull(file);
 		Assert.assertTrue(file.exists());
-		
 	}
 	
 }

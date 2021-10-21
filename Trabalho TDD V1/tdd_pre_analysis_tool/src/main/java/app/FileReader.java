ac0fdeb0f11b09485768edf8b061e257fdb50705
gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
2021-10-20 23:46:34 -0300

    adding column conversion
    Co-authored-by: gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
    Co-authored-by: filypsdias <filipedias1708@gmail.com>
    Co-authored-by: pAmanda <amandoca_779@hotmail.com>

--- Trabalho TDD V1/tdd_pre_analysis_tool/src/main/java/app/FileReader.java
+++ Trabalho TDD V1/tdd_pre_analysis_tool/src/main/java/app/FileReader.java
@@ -6,6 +6,9 @@ import main.java.app.exception.DisposicaoInvalidaException;
 import main.java.app.exception.EscritaNaoPermitidaException;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class FileReader {
 	
@@ -50,33 +53,78 @@ public class FileReader {
 		BufferedReader br = new BufferedReader(new java.io.FileReader(file));
 		BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath, false));
 		
-		if ("linhas".equalsIgnoreCase(disposition)) {
-			while ((st = br.readLine()) != null) {
-				
-				if (!st.contains("-")) {
-					writer.append(delimiter);
-					writer.append(st);
-				} else {
-					final String pattern = "[^0-9]";
-					final String emptyReplacement = "";
+
+		while ((st = br.readLine()) != null) {
+
+			if (!st.contains("-")) {
+				writer.append(delimiter);
+				writer.append(st);
+			} else {
+				final String pattern = "[^0-9]";
+				final String emptyReplacement = "";
 					
-					if (counter > 1 && st.contains("-")) {
-						writer.append(System.lineSeparator());
-						evolution = st.replaceAll(pattern, emptyReplacement);
-						writer.append(evolution);
-						counter++;
-					} else if (counter == 1) {
-						evolution = st.replaceAll(pattern, emptyReplacement);
-						writer.append(evolution);
-						counter++;
-					}
+				if (counter > 1 && st.contains("-")) {
+					writer.append(System.lineSeparator());
+					evolution = st.replaceAll(pattern, emptyReplacement);
+					writer.append(evolution);
+					counter++;
+				} else if (counter == 1) {
+					evolution = st.replaceAll(pattern, emptyReplacement);
+					writer.append(evolution);
+					counter++;
 				}
 			}
-			writer.close();
-			
-		} else {
-			System.out.println("\nDesculpe, mas no momento só estamos processando diposição por linhas!");
 		}
+
+		writer.close();
+
+		if ("colunas".equals(disposition)) {
+
+			Scanner s = new Scanner(new File(outputPath));
+			ArrayList< String > arrayDeLinhas = new ArrayList < String > ();
+			while (s.hasNextLine()) {
+				arrayDeLinhas.add(s.nextLine());
+			}
+			s.close();
+
+			int i = 0;
+			int j = 0;
+			String[][] newArray = new String[100][100];
+
+			for (String linha: arrayDeLinhas) {
+				for (String palavra: linha.split(delimiter)) {
+					newArray[i][j] = palavra;
+					j++;
+				}
+				i++;
+				j = 0;
+			}
+
+			System.out.println(Arrays.toString(newArray[0]));
+			System.out.println(Arrays.toString(newArray[1]));
+
+			file = new File(path);
+			writer = new BufferedWriter(new FileWriter(outputPath, false));
+
+			i = 0;
+			j = 0;
+
+			for (j = 0; j < 100; j++) {
+				for (i = 0; i < 100; i++) {
+					if ((newArray[i][j] != null)) {
+						writer.append(newArray[i][j]);
+						writer.append(delimiter);
+					} else {
+						writer.append("");
+					}
+
+				}
+				writer.append(System.lineSeparator());
+			}
+			writer.close();
+		}
+			
+
 		
 	}
 	

bb2af286c47e87e78d81a56c57f0a20ee95b4afc
Filipe Dias <lipao.dias@hotmail.com>
2021-10-28 19:25:47 -0300

    [Refact] Remove Unused variables & Extract Methods
    Origem: src/main/java/app/FileReader.java
    Impactos: FileReader.java
    Explicação: Manter o princípio SOLID (Single Responsibiliy Principle) e
    extrair métodos em trechos de código que o resultado da transformação de
    dados é escrito em arquivo. Também foram removidas variáveis não
    utilizadas.  Também fora realizada a extração de objeto-método, ou seja,
    métodos de escrita e transformação de dados são chamados em um método de
    indireção (writeOnFile FileReader.java.37:5). Tais métodos são chamados
    de readAndWriteInLineDirection e readAndWriteInColumnDirection
    
    Co-authored-by: gustavocarvalho1002 <gustavocarvalho1002@gmail.com>
    Co-authored-by: pAmanda <amandoca_779@hotmail.com>

--- Trabalho TDD V1/tdd_pre_analysis_tool/src/main/java/app/FileReader.java
+++ Trabalho TDD V1/tdd_pre_analysis_tool/src/main/java/app/FileReader.java
@@ -20,7 +20,7 @@ public class FileReader {
 		File file = new File(path);
 		
 		//verify if file exists
-		existentFileVerifier(path, file);
+		verifyIsExistentFile(path, file);
 
 		//verify delimiter length
 		verifyInvalidDelimiterLength(delimiter);
@@ -31,87 +31,103 @@ public class FileReader {
 		//verify if file is writable
 		verifyIsWritableFile(path, outputPath);
 
-		String st;
-		String evolution;
-		
+		writeOnFile(path, delimiter, disposition, outputPath, file);
+	}
+
+	private void writeOnFile(final String path,
+							 final String delimiter,
+							 final String disposition,
+							 final String outputPath,
+							 File file) throws IOException {
+
 		int counter = 1;
-		
-		BufferedReader br = new BufferedReader(new java.io.FileReader(file));
+
+		final String COLUMN_DELIMITER_NAME = "colunas";
+
+		BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputPath, false));
+
+		readAndWriteInLineDirection(delimiter, bufferedReader, bufferedWriter, counter); //TODO change name of this method
+
+		if (COLUMN_DELIMITER_NAME.equals(disposition)) {
+			readAndWriteColumnDirection(path, delimiter, outputPath);
+		}
+	}
+
+	private void readAndWriteColumnDirection(String path, String delimiter, String outputPath) throws IOException {
+
+		Scanner scanner = new Scanner(new File(outputPath));
+		ArrayList<String> linesArray = new ArrayList<>();
+
+		while (scanner.hasNextLine()) {
+			linesArray.add(scanner.nextLine());
+		}
+		scanner.close();
+
+		int i = 0; //TODO refactor this variable name
+		int j = 0; //TODO refactor this variable name
+
+		String[][] newArray = new String[100][100];
+
+		for (String linha: linesArray) {
+			for (String palavra: linha.split(delimiter)) {
+				newArray[i][j] = palavra;
+				j++;
+			}
+			i++;
+			j = 0;
+		}
+
 		BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath, false));
-		
 
-		while ((st = br.readLine()) != null) {
+		for (j = 0; j < 100; j++) {
+			for (i = 0; i < 100; i++) {
+				if ((newArray[i][j] != null)) {
+					writer.append(newArray[i][j]);
+					writer.append(delimiter);
+				} else {
+					writer.append("");
+				}
 
-			if (!st.contains("-")) {
-				writer.append(delimiter);
-				writer.append(st);
+			}
+			writer.append(System.lineSeparator());
+		}
+		writer.close();
+	}
+
+	private void readAndWriteInLineDirection(final String delimiter,
+											 final BufferedReader bufferedReader,
+											 final BufferedWriter bufferedWriter,
+											 int counter) throws IOException {
+
+		String currentLine;
+		String evolution;
+
+		while ((currentLine = bufferedReader.readLine()) != null) {
+			final boolean isNotEvolutionHeaderLine = !currentLine.contains("-");
+
+			if (isNotEvolutionHeaderLine) {
+				bufferedWriter.append(delimiter);
+				bufferedWriter.append(currentLine);
 			} else {
-				final String pattern = "[^0-9]";
-				final String emptyReplacement = "";
-					
-				if (counter > 1 && st.contains("-")) {
-					writer.append(System.lineSeparator());
-					evolution = st.replaceAll(pattern, emptyReplacement);
-					writer.append(evolution);
+				final String PATTERN = "[^0-9]";
+				final String EMPTY_REPLACEMENT = "";
+
+				if (counter > 1 && currentLine.contains("-")) {
+					bufferedWriter.append(System.lineSeparator());
+					evolution = currentLine.replaceAll(PATTERN, EMPTY_REPLACEMENT);
+					bufferedWriter.append(evolution);
+
 					counter++;
 				} else if (counter == 1) {
-					evolution = st.replaceAll(pattern, emptyReplacement);
-					writer.append(evolution);
+					evolution = currentLine.replaceAll(PATTERN, EMPTY_REPLACEMENT);
+					bufferedWriter.append(evolution);
+
 					counter++;
 				}
 			}
 		}
-
-		writer.close();
-
-		if ("colunas".equals(disposition)) {
-
-			Scanner s = new Scanner(new File(outputPath));
-			ArrayList< String > arrayDeLinhas = new ArrayList < String > ();
-			while (s.hasNextLine()) {
-				arrayDeLinhas.add(s.nextLine());
-			}
-			s.close();
-
-			int i = 0;
-			int j = 0;
-			String[][] newArray = new String[100][100];
-
-			for (String linha: arrayDeLinhas) {
-				for (String palavra: linha.split(delimiter)) {
-					newArray[i][j] = palavra;
-					j++;
-				}
-				i++;
-				j = 0;
-			}
-
-			System.out.println(Arrays.toString(newArray[0]));
-			System.out.println(Arrays.toString(newArray[1]));
-
-			file = new File(path);
-			writer = new BufferedWriter(new FileWriter(outputPath, false));
-
-			i = 0;
-			j = 0;
-
-			for (j = 0; j < 100; j++) {
-				for (i = 0; i < 100; i++) {
-					if ((newArray[i][j] != null)) {
-						writer.append(newArray[i][j]);
-						writer.append(delimiter);
-					} else {
-						writer.append("");
-					}
-
-				}
-				writer.append(System.lineSeparator());
-			}
-			writer.close();
-		}
-			
-
-		
+		bufferedWriter.close();
 	}
 
 	private void verifyIsWritableFile(final String path, final String outputPath) throws IOException,
@@ -145,7 +161,7 @@ public class FileReader {
 		}
 	}
 
-	private void existentFileVerifier(final String path, final File file) throws ArquivoNaoEncontradoException {
+	private void verifyIsExistentFile(final String path, final File file) throws ArquivoNaoEncontradoException {
 		if (!file.exists()) {
 			throw new ArquivoNaoEncontradoException("File not found on path: " + path);
 		}

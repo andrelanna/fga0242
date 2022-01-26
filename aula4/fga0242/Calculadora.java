c3ab6cff38dd19e5c06f11281736152c1e14283f
Andre Lanna <andre.lanna@gmail.com>
2022-01-26 17:11:53 -0300

    [Aula 4] Enunciado do exercicio alterado e código feito em sala disponibilizado

---
+++ aula4/fga0242/Calculadora.java
@@ -0,0 +1,25 @@
+package projetoTesteParametrizado;
+
+public class Calculadora {
+
+	private int op1;
+	private int op2;
+	private int resultado;
+
+	public int subtracao(int op1, int op2) {
+		this.op1 = op1; 
+		this.op2 = op2;
+		this.resultado = op1 - op2; 
+		
+		return this.resultado;
+	}
+
+	public int soma(int op1, int op2) {
+		this.op1 = op1; 
+		this.op2 = op2; 
+		this.resultado = op1 + op2;
+		
+		return this.resultado;
+	}
+
+}

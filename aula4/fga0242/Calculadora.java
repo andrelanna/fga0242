940fee3d0e906f6a672cf39688cc513bb08a8c9b
Marcus Martins <89209017+marcusmartinss@users.noreply.github.com>
2025-04-17 02:48:54 +0000

    Correção do arquivo de Calculadora.java
    Havia feito os commits diretamente pelo site do GitHub e acabei confundindo os arquivos na hora de alterar e subi no arquivo de Calculadora o que deveria ser de TesteSubtracaoSimples.

--- aula4/fga0242/Calculadora.java
+++ aula4/fga0242/Calculadora.java
@@ -1,41 +1,26 @@
 
 package fga0242;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+public class Calculadora {
 
-public class TesteSubtracaoSimples {
+	private int op1;
+	private int op2;
+	private int resultado;
 
-	Calculadora c; 
-	
-	@BeforeEach
-	public void setup() {
-		c = new Calculadora();
+	public int subtracao(int op1, int op2) {
+		this.op1 = op1; 
+		this.op2 = op2;
+		this.resultado = op1 - op2; 
+		
+		return this.resultado;
 	}
-	
-	@Test
-	public void test1() { //3-2
-		assertEquals(1, c.subtracao(3,2));
+
+	public int soma(int op1, int op2) {
+		this.op1 = op1; 
+		this.op2 = op2; 
+		this.resultado = op1 + op2;
+		
+		return this.resultado;
 	}
-	
-	@Test
-	public void test2() { //2-3
-		assertEquals(-1, c.subtracao(2,3));
-	}
-	
-	@Test
-	public void test3() { //3-(-2)
-		assertEquals(5, c.subtracao(3,-2));
-	}
-	
-	@Test
-	public void test4() { //2-(-3)
-		assertEquals(5, c.subtracao(2,-3));
-	}
-	
-	@Test
-	public void test5() { //-3-(-2)
-		assertEquals(-1, c.subtracao(-3,-2));
-	}
+
 }
